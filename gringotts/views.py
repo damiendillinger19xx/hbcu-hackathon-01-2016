075f4032a730be50d981eb4930be2c7c6106966d
Roshan Thapaliya <roshan.thapaliya@bison.howard.edu>
2015-10-11 01:42:39 -0400

    Working on transfer

--- gringotts/views.py
+++ gringotts/views.py
@@ -2,18 +2,18 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import json
 import urllib2
-import requests
+
 
 
 # Create your views here.
 
 reimagine_api_key = 'key=73773e58efaba48db97f6f32c3f89f51'
-all_clients_api = 'http://api.reimaginebanking.com/accounts?type=Checking&'
+all_clients_api = 'http://api.reimaginebanking.com/accounts?'
 transfer_api = 'http://api.reimaginebanking.com/accounts/%s/transfers?'
 
 
 def index_view(request):
-	return render(request, 'gringotts/base.html')
+	return render(request, 'gringotts/base.html',{"key":"73773e58efaba48db97f6f32c3f89f51"})
 
 
 def all_clients_view(request):
@@ -26,14 +26,14 @@ def all_clients_view(request):
 
 
 def transfer(request):
+	if request.POST:
+		person_id = request.POST['id']
+		amount = request.POST['money']
+		send = 'http://api.reimaginebanking.com/accounts/%s/transfers?key=73773e58efaba48db97f6f32c3f89f51'%(person_id)
+		page = urllib2.urlopen(send) 
+		return HttpResponse(amount+person_id)
+		 
 	pass
- 	# if request.GET:
- 	# 	payee_id = request.GET['id']
- 	# 	transfer = transfer_api + reimagine_api_key
- 	# 	page = urllib2.urlopen(transfer % (x))
- 	# 	page_read = page.read()
- 	# 	page = json.loads(page_read)
- 	# return render(request, 'gringotts/base.html')
 
 		
 
@@ -42,3 +42,4 @@ def transfer(request):
 # page = urllib2.urlopen(data)
 # page_read = page.read()
 # page = json.loads(page_read)
+#http://api.reimaginebanking.com/accounts/rtyuuyt/transfers?key=73773e58efaba48db97f6f32c3f89f51
